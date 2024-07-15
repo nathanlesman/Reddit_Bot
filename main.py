b02cf59573a7b71c598f3423a65be0c9d59c9622
Nathan <147553061+nathanlesman@users.noreply.github.com>
2024-07-16 00:04:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import praw
+import os
+from psaw import PushshiftAPI
+from replit import db
 
+client_id = os.environ['client_id'] 
+client_secret = os.environ['client_secret']  
+username = os.environ['username'] 
+password = os.environ['password']
